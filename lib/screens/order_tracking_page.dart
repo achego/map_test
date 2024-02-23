526d75868e2e381798f1a742046de6d365a58e78
belema <tamunobelemaachego@gmail.cpm>
2024-02-23 22:44:48 +0100

    settings

--- lib/screens/order_tracking_page.dart
+++ lib/screens/order_tracking_page.dart
@@ -1,4 +1,4 @@
-import 'dart:async';
+import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -11,17 +11,32 @@ class OrderTrackingPage extends StatefulWidget {
 }
 
 class _OrderTrackingPageState extends State<OrderTrackingPage> {
-  final Completer<GoogleMapController> mapController = Completer();
+  // final Completer<GoogleMapController> mapController = Completer();
+  late GoogleMapController mapController;
+
   static const LatLng source = LatLng(37.33500926, -122.03272188);
   static const LatLng destination = LatLng(37.33, -122.03);
+  final LatLng center = const LatLng(-23.5557714, -46.6395571);
+
+  _onMapCreated(GoogleMapController controller) {
+    logger('Map don create o');
+    logger('The Controller ', message: controller.mapId);
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Order Tracking things')),
       body: GoogleMap(
-        initialCameraPosition: const CameraPosition(target: source, zoom: 14.4),
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(target: center, zoom: 11),
         markers: {const Marker(markerId: MarkerId('source'), position: source)},
       ),
     );
   }
+}
+
+logger(String title, {dynamic message}) {
+  log("[$title] ===== $message");
 }
